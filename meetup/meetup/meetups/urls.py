aba51376922e5445e4fa456dd33c922cb6263281
codify21 <mac95shivam@gmail.com>
2022-05-31 20:47:44 +0530

    django project meetup dynamicc slug in url

--- meetup/meetup/meetups/urls.py
+++ meetup/meetup/meetups/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('meetups/',views.index), #add / at the end as meetups/ for avoiding 404 error
-    path('meetups/<slug:meetup_slug>',views.meetup_details)
+    path('meetups/',views.index,name='all-meetups'), #add / at the end as meetups/ for avoiding 404 error
+    path('meetups/<slug:meetup_slug>',views.meetup_details,name='meetup-detail')
 ]
