dabbd765cd35962b20340b1e7ad6b836b4b094bd
codify21 <mac95shivam@gmail.com>
2022-06-03 03:04:01 +0530

    multiple model many to many relationship

--- meetup/meetup/meetups/admin.py
+++ meetup/meetup/meetups/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Meetup,Location
+from .models import Meetup,Location,Participant
 # Register your models here.
 
 class MeetupAdmin(admin.ModelAdmin):
@@ -9,3 +9,4 @@ class MeetupAdmin(admin.ModelAdmin):
 
 admin.site.register(Meetup,MeetupAdmin)
 admin.site.register(Location)
+admin.site.register(Participant)
--- meetup/meetup/meetups/models.py
+++ meetup/meetup/meetups/models.py
@@ -9,6 +9,12 @@ class Location(models.Model):
     def __str__(self):
         return  f'{self.name}  ({self.address})'
 
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
 class Meetup(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
@@ -16,6 +22,8 @@ class Meetup(models.Model):
     # location = models.CharField(max_length=100,default='India')
     locationn = models.ForeignKey(Location, on_delete=models.CASCADE)#model.SET_NULL also can be used
     image = models.ImageField(upload_to ='images')
+    participantss = models.ManyToManyField(Participant,blank=True,null=True)
+    # Without null= True, if u have blank = True , for some fileds(like CharField) default empty values
 
     def __str__(self):
         return f'{self.title}-{self.slug}'
