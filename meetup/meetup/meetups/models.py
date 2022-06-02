759c1dc5aa40c6c21de40f86acb68a5fd3d689c7
codify21 <mac95shivam@gmail.com>
2022-06-02 14:52:22 +0530

    django project meetup using models to create and get data

--- meetup/meetup/meetups/models.py
+++ meetup/meetup/meetups/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Meetup(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    location = models.CharField(max_length=100,default='India')
--- meetup/meetup/meetups/views.py
+++ meetup/meetup/meetups/views.py
@@ -1,33 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Meetup
 
 # Create your views here.
 
 def index(request):
-    meetups =[
-        {'title':'A First meetup',
-        'location':'New York',
-        'slug':'a-first-meetup'
-        },
-        {'title':'A second meetup',
-        'location':'Paris',
-        'slug':'a-second-meetup'
-        },
-        {'title':'A third meetup',
-        'location':'Mexico',
-        'slug':'a-third-meetup'
-        }
-    ]
+    
+    meetups = Meetup.objects.all()
+    
+    # meetups =[
+    #     {'title':'A First meetup',
+    #     'location':'New York',
+    #     'slug':'a-first-meetup'
+    #     },
+    #     {'title':'A second meetup',
+    #     'location':'Paris',
+    #     'slug':'a-second-meetup'
+    #     },
+    #     {'title':'A third meetup',
+    #     'location':'Mexico',
+    #     'slug':'a-third-meetup'
+    #     }
+    # ]
     return render(request,'meetups/index.html',{
         'show_meetups': True,
         'meetups':meetups})
 
 def meetup_details(request,meetup_slug):
-    selected_item ={ 
-        'title':'A First Meetup',
-        'description':'This is the first meetup'
-        }
-    return render(request,'meetups/meetup-details.html',{
-        'meetup_title':selected_item['title'],
-        'meetup_description':selected_item['description']
-    })
+     # selected_item ={ 
+    #     'title':'A First Meetup',
+    #     'description':'This is the first meetup'
+    #     }
+    try:
+        selected_item =  Meetup.objects.get(slug= meetup_slug)
+        return render(request,'meetups/meetup-details.html',{
+            # 'meetup_title':selected_item['title'],
+            'meetup_title':selected_item.title,
+            # 'meetup_description':selected_item['description']
+            'meetup_description':selected_item.description,
+            'meetup_found':True,
+        })
+    except Exception as exc:
+         return render(request,'meetups/meetup-details.html',{
+          'meetup_found':False,
+         })
+
