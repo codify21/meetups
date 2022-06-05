63a4fe4b4f250dcbd0004c15d253ea95eabcfcbe
codify21 <mac95shivam@gmail.com>
2022-06-05 20:09:39 +0530

    optimizing urls

--- meetup/meetup/meetups/urls.py
+++ meetup/meetup/meetups/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('meetups/',views.index,name='all-meetups'), #add / at the end as meetups/ for avoiding 404 error
-    path('meetups/<slug:meetup_slugg>/success',views.confirm_registration,name='confirm-registration'),
-    path('meetups/<slug:meetup_slug>',views.meetup_details,name='meetup-detail')
+    path('',views.index,name='all-meetups'), #add / at the end as meetups/ for avoiding 404 error
+    path('<slug:meetup_slugg>/success',views.confirm_registration,name='confirm-registration'),
+    path('<slug:meetup_slug>',views.meetup_details,name='meetup-detail')
 ]
