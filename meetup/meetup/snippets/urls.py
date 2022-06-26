8f9df3df6c4ed10c52b5e72441b2a020297a3804
codify21 <mac95shivam@gmail.com>
2022-06-27 03:00:36 +0530

    Final snippets working code

--- meetup/meetup/snippets/urls.py
+++ meetup/meetup/snippets/urls.py
@@ -8,6 +8,29 @@ from rest_framework.urlpatterns import format_suffix_patterns
 #     path('<int:pk>/', views.snippet_detail),
 # ]
 
+# from snippets.views import SnippetViewSet, UserViewSet, api_root
+# from rest_framework import renderers
+
+# snippet_list = SnippetViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create'
+# })
+# snippet_detail = SnippetViewSet.as_view({
+#     'get': 'retrieve',
+#     'put': 'update',
+#     'patch': 'partial_update',
+#     'delete': 'destroy'
+# })
+# snippet_highlight = SnippetViewSet.as_view({
+#     'get': 'highlight'
+# }, renderer_classes=[renderers.StaticHTMLRenderer])
+# user_list = UserViewSet.as_view({
+#     'get': 'list'
+# })
+# user_detail = UserViewSet.as_view({
+#     'get': 'retrieve'
+# })
+
 urlpatterns = [
     path('', views.api_root),
     path('snippets/', views.SnippetList.as_view(),name='snippet-list'),
@@ -18,4 +41,18 @@ urlpatterns = [
   
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
+# from snippets import views
+
+# # Create a router and register our viewsets with it.
+# router = DefaultRouter()
+# router.register(r'snippets', views.SnippetViewSet,basename="snippets")
+# router.register(r'users', views.UserViewSet,basename="users")
+
+# # The API URLs are now determined automatically by the router.
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
